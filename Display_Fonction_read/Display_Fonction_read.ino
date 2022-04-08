68bb27bde7a70625cfb192201095e9a96c6ba87e
Harold0Finch <schneider.antoine57@outlook.fr>
2022-04-08 10:58:02 +0200

    Update Display_Fonction_read.ino
    Rajout de commentaires

--- Display_Fonction_read/Display_Fonction_read.ino
+++ Display_Fonction_read/Display_Fonction_read.ino
@@ -3,14 +3,13 @@
 
 // Defines the LCD's parameters: (rs, enable, d4, d5, d6, d7)
 LiquidCrystal lcd(9, 8, 5, 4, 3, 2);
-int valeur = 0; // Variable où on stock la valeur du potentiomètre
-float tension = 0;
+int valeur = 0;     // Variable où on stock la valeur du potentiomètre
+float tension = 0;  // Variable où on stock la valeur de la tension
 
 void setup() {
   
-  // Set up the LCD's number of columns and rows
-  lcd.begin(16, 2);
-  Serial.begin(9600);  // Initialisons la communication serial
+  lcd.begin(16, 2); // Initialisation du LCD
+  Serial.begin(9600);  // Initialison de la communication serie
   
 }
 
@@ -18,14 +17,12 @@ void loop() {
 
   valeur = analogRead(A0); // on lit les données du pin A0 
   tension = (valeur*5.00/1023.00);
-  // Prints the introductory message
-  Serial.print("Tension : ");
+  Serial.print("Tension : "); // Impression du message sur le moniteur série
   Serial.print(tension);
   Serial.println(" Volts");
-  // Prints the first part of the message to the LCD
-  lcd.print(tension);
+  lcd.print(tension); // Impression du message sur le LCD
   lcd.print(" V");
-  delay(1000);
-  lcd.clear();
+  delay(1000); // Attente de 1 sec entre chaque rafraichissement
+  lcd.clear(); // On efface le message sur le LCD
   
 }
